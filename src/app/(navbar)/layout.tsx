6a64fbf4402a0f69a456889c360b53ccad7ba704
Lam Nguyen <lam@southernmobile.com.au>
2023-10-23 23:14:08 +0700

    Backup redux

--- src/app/(navbar)/layout.tsx
+++ src/app/(navbar)/layout.tsx
@@ -1,6 +1,9 @@
 import MainNavbarClientWrapper from "./NavbarClientWrapper";
 
 export default function NavLayout({ children }: { children: React.ReactNode }) {
+  if (typeof window !== "undefined") {
+    return <div />;
+  }
   return (
     <main>
       <div className="w-full h-16 bg-black">
--- src/app/(navbar)/login/LoginForm.tsx
+++ src/app/(navbar)/login/LoginForm.tsx
@@ -2,7 +2,7 @@
 
 import { useAppDispatch, useAppSelector } from "@/app/store";
 import { login } from "@/app/store/slices/authSlice";
-import { useCallback, useState } from "react";
+import { MouseEventHandler, useCallback, useState } from "react";
 import useApi from "@/hooks/api/useApi";
 
 export default function LoginForm() {
@@ -10,21 +10,18 @@ export default function LoginForm() {
   const dispatch = useAppDispatch();
   const { previousUrl } = useAppSelector((state) => state.auth);
 
-  const handleSubmit = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      await dispatch(login(api, previousUrl || "http://localhost:3000/home"));
-    },
-    [api, dispatch, previousUrl]
-  );
+  const handleSubmit = useCallback(async () => {
+    await dispatch(login(api, previousUrl || "http://localhost:3000/home"));
+  }, [api, dispatch, previousUrl]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <button type="submit" className="bg-blue-700 p-2 text-white font-bold">
-          Login with TMDB
-        </button>
-      </div>
-    </form>
+    <div>
+      <button
+        onClick={handleSubmit}
+        className="bg-blue-700 p-2 text-white font-bold"
+      >
+        Login with TMDB
+      </button>
+    </div>
   );
 }
