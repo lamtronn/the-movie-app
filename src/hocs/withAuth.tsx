47a63f36c31ddda5048d15bed8dfc382c75fa6e5
Lam Nguyen <lam@southernmobile.com.au>
2023-10-25 23:56:41 +0700

    Update withAuth

--- src/hocs/withAuth.tsx
+++ src/hocs/withAuth.tsx
@@ -45,18 +45,21 @@ const withAuth = (WrappedComponent: any) => {
       ).state;
 
       // eslint-disable-next-line react-hooks/rules-of-hooks
-      useEffect(() => {
-        if (
-          authStateLocalStorage.requestToken &&
-          !authStateLocalStorage.accessToken
-        ) {
-          getAccessToken();
-        }
-      }, [
-        authStateLocalStorage.accessToken,
-        authStateLocalStorage.requestToken,
-        getAccessToken,
-      ]);
+      useEffect(
+        () => {
+          if (
+            authStateLocalStorage.requestToken &&
+            !authStateLocalStorage.accessToken
+          ) {
+            getAccessToken();
+          }
+        },
+        [
+          // authStateLocalStorage.accessToken,
+          // authStateLocalStorage.requestToken,
+          // getAccessToken,
+        ],
+      );
 
       if (
         authStateLocalStorage.isLoadingAccessToken &&
